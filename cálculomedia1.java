b7b8e8fa733678df222eeee9bd562161ac94353d
yngridandrade26 <leidianacav@gmail.com>
2025-10-23 21:16:19 -0300

    Create cálculomedia1.java

---
+++ cálculomedia1.java
@@ -0,0 +1,10 @@
+public class calculomedia{
+    public static void main (string [] args){
+        system.out.println("---Sistema de notas---");
+        double nota1 = 8.0;
+        double nota2 = 6.5;
+        double nota3 = 7.5;
+        double media = (nota1+nota2+nota3)/3;
+        system.out.println("A média do aluno é:" +média);
+    }
+}
